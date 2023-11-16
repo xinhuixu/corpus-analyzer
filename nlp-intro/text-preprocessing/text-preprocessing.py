d228e5f1ce36335d9b7e7829ca16875f739ad532
xinhuixu <x.xu.1102@gmail.com>
2023-11-16 13:50:48 -0500

    extend to web app

--- nlp-intro/text-preprocessing/text-preprocessing.py
+++ nlp-intro/text-preprocessing/text-preprocessing.py
@@ -8,6 +8,36 @@ from nltk.stem import WordNetLemmatizer
 # grabbing a part of speech function:
 from part_of_speech import get_part_of_speech
 
+# web app
+from flask import Flask, render_template, request # Import Flask and related modules
+app = Flask(__name__) # Create a Flask web application
+
+@app.route('/')  # Define a route for the home page
+def index():
+    return render_template('index.html')  # Render the HTML template for the home page
+
+@app.route('/preprocess_text', methods=['POST'])  # Define a route for processing text when the form is submitted
+def preprocess_text():
+    if request.method == 'POST':  
+        text = request.form['text']  # Get the text from the submitted form
+        # Preprocess the text
+        cleaned = re.sub('\W+', ' ', text)
+        tokenized = word_tokenize(cleaned)
+
+        stemmer = PorterStemmer()
+        stemmed = [stemmer.stem(token) for token in tokenized]
+
+        lemmatizer = WordNetLemmatizer()
+        lemmatized = [lemmatizer.lemmatize(token, get_part_of_speech(token)) for token in tokenized]
+
+        # Render the HTML template with the original text, stemmed text, and lemmatized text
+        return render_template('index.html', original_text=text, stemmed_text=stemmed, lemmatized_text=lemmatized)
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the Flask app in debug mode if executed directly
+
+
+'''
 text = "So many squids are jumping out of suitcases these days that you can barely go anywhere without seeing one burst forth from a tightly packed valise. I went to the dentist the other day, and sure enough I saw an angry one jump out of my dentist's bag within minutes of arriving. She hardly even noticed."
 
 cleaned = re.sub('\W+', ' ', text)
@@ -24,3 +54,4 @@ print("Stemmed text:")
 print(stemmed)
 print("\nLemmatized text:")
 print(lemmatized)
+'''
